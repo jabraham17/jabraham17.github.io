0ae1ee4c97cf77a7195503f110d50b461c609f8a
Jacob Abraham <jacob.r.abraham@gmail.com>
2020-06-07 22:33:45 -0700

    lots of formating tweaks, add content to apps and about

--- resources/header/header.js
+++ resources/header/header.js
@@ -5,9 +5,15 @@ export function buildHeader(headerText = document.title) {
   let header = document.createElement("header");
   header.className = "page_header";
 
+  let headerContent = document.createElement("div");
+  headerContent.className = "header_content";
+
   let icon = document.createElement("img");
   icon.src = "/resources/icons/favicon.png";
   icon.className = "header_icon"
+
+  let rightBar = document.createElement("div");
+  rightBar.className = "header_right_bar"
 
   let text = document.createElement("h1");
   text.innerHTML = headerText;
@@ -16,9 +22,13 @@ export function buildHeader(headerText = document.title) {
   let navBar = document.createElement("nav");
   buildNavBar(navBar);
 
-  header.appendChild(icon);
-  header.appendChild(text);
-  header.appendChild(navBar);
+  rightBar.appendChild(text);
+  rightBar.appendChild(navBar);
+
+  headerContent.appendChild(icon);
+  headerContent.appendChild(rightBar);
+
+  header.appendChild(headerContent)
 
   document.body.insertBefore(header, document.body.firstChild)
 }
